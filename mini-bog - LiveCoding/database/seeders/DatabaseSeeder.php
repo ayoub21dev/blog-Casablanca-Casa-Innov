fdc8bc137c92b7d4164a096fcbc6b693de03b841
ayoub21dev <Ayoubcode941@gmail.com>
2025-11-07 08:44:00 +0000

    update()

---
+++ mini-bog - LiveCoding/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       
+        $tags = Tag::factory(10)->create();
+
+        User::factory(5)
+            ->has(
+                Article::factory(10)
+                    ->hasAttached($tags->random(3))
+            )
+            ->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
